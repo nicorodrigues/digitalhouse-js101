1c0bb33996f391241a97973749726b102e84dc0e
Nicolás Rodrigues <git@nicorodrigues.com.ar>
2019-12-09 16:54:09 -0300

    Ejercitaciones

---
+++ ejercitacion/01-prototype-inicial.js
@@ -0,0 +1,6 @@
+/**
+ * 1. Crear un prototype llamado SuperHeroe
+ * 2. Permitir que los SuperHeroes tengan nombre, identidad secreta, un nivel de poder (del 1 al 100) y 100 de vida
+ * 3. Crear un método en el prototype que permita que un heroe golpee a otro y le reste vida
+ * 4. Mostrar el proceso usando console.log en consola
+ */
---
+++ ejercitacion/02-prototype-avanzado.js
@@ -0,0 +1,8 @@
+/**
+ * 1. Crear dos salones de clases con número de aula y profesor (solo nombre del profesor)
+ * 2. Crear varios alumnos
+ * 3. Crear una propiedad del salón de clase que permita que contenga varios alumnos
+ * 4. Agregar alumnos a las aulas
+ * 5. Crear una funcion en el prototype de las aulas que les permita pasar asistencia (console.log de los alumnos del aula)
+ * 6. Modificar la funcion para que un aula tome asistencia en orden alfabético (ordenando los nombres por primera letra de la A a la Z) y la otra lo haga al revés
+ */
